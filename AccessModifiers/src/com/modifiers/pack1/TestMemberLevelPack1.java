18ba582036c8d8c953942d22897caef97e04ff68
dfavo <favorskyd@gmail.com>
2019-10-02 14:47:28 -0700

    Add file AccessModifiers

---
+++ AccessModifiers/src/com/modifiers/pack1/TestMemberLevelPack1.java
@@ -0,0 +1,19 @@
+package com.modifiers.pack1;
+
+public class TestMemberLevelPack1
+{
+	public static void main(String[] args)
+	{
+		MemberLevel ml = new MemberLevel("Steinbeck", "John", 68, "Salinas");
+		
+//		ml.privateLastName = "Abc";
+		ml.publicFirstName = "Anthony";
+		ml.protectedAge = 24;
+		ml.defaultCity = "San Francisco";
+		
+//		ml.getPrivateLastName();
+		ml.getPublicFirstName();
+		ml.getProtectedAge();
+		ml.getDefaultCity();
+	}
+}
---
+++ AccessModifiers/src/com/modifiers/pack2/MemberLevel2.java
@@ -0,0 +1,29 @@
+package com.modifiers.pack2;
+
+import com.modifiers.pack1.*;
+
+public class MemberLevel2 extends MemberLevel
+{
+
+	public MemberLevel2(String ln, String fn, int ag, String ci)
+	{
+		super(ln, fn, ag, ci);
+	}
+	
+	public void usingSuperclassVariables()
+	{
+//		privateLastName = "Reagan";
+		publicFirstName = "Ronald";
+		protectedAge = 88;
+//		defaultCity = "DC";
+	}
+	
+	public void usingSuperclassMethods()
+	{
+//		getPrivateLastName();
+		getPublicFirstName();
+		getProtectedAge();
+//		getDefaultCity();
+	}
+		
+}
